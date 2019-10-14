ec296604801ff97a54e969a05c25ddada30f6dba
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2019-10-15 00:09:43 +0200

    Tid_2

--- src/TimeTesting.java
+++ src/TimeTesting.java
@@ -10,16 +10,26 @@ public class TimeTesting {
     int m2 = 30;
     int s2 = 20;
 
+    int h3 = 12;
+    int m3 = 20;
+    int s3 = 30;
+
 
     public void method() {
 
-        LocalTime localTime1 = LocalTime.of(h1, m1, s1);
-        LocalTime localTime2 = LocalTime.of(h2, m2, s2);
+        LocalTime film_1_før = LocalTime.of(h1, m1, s1);
+        LocalTime film_1_efter = LocalTime.of(h2, m2, s2);
+        LocalTime film_2_før = LocalTime.of(h3, m3, s3);
 
-        System.out.println("Tid 1 er " + localTime1);
-        System.out.println("Tid 2 er " + localTime2);
+        System.out.println("Film 1 starter klokken " + film_1_før);
+        System.out.println("Film 1 slutter klokken " + film_1_efter);
+        System.out.println("Film 2 starter klokken " + film_2_før);
 
-        System.out.println(localTime1.isAfter(localTime2));
+        //Her testes om den planlagte film vil slutte før den næste film skal på - hvis ikke vil den fejle
+
+        System.out.println("\n" + film_1_efter.isBefore(film_2_før));
+
+        System.out.println(film_1_efter.isAfter(film_2_før));
 
     }
 
